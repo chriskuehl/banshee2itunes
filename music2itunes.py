87a3ee72c4e9fe1b5785590197f48243bdadf081
Chris Kuehl <chris@techxonline.net>
2013-09-21 09:20:06 -0700

    Add method for generating an M3U from a folder.

---
+++ music2itunes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import sys, os, os.path
+
+def make_m3u(path):
+	m3u = "#EXTM3U\n\n"
+
+	for file in os.listdir(path):
+		filePath = path + "/" + file
+		ext = os.path.splitext(filePath)[1]
+
+		if ext in [".mp3", ".m4a", ".wav", ".aif"]:
+			m3u += "#EXTINF,0,{}\n{}\n\n".format(filePath, filePath)
+	
+	return m3u
+
+# Generate an M3U playlist with all of the files
+m3u = make_m3u(os.path.expanduser("~/Desktop/music"))
+print(m3u)
