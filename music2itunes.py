e9c55c15daeed6f530591e9fb36d4b3d594b6208
Chris Kuehl <chris@techxonline.net>
2013-09-21 14:30:09 -0700

    Changes to saving iTunes.

--- music2itunes.py
+++ music2itunes.py
@@ -13,6 +13,31 @@ def make_m3u(path):
 	
 	return m3u
 
+# kill iTunes
+os.system("killall iTunes 2>/dev/null")
+
+# remove the iTunes library files
+files = ["iTunes Music Library.xml", "iTunes Library.itl"]
+
+for file in files:
+	path = os.path.expanduser("~/Music/iTunes/" + file)
+	
+	if os.path.isfile(path):
+		os.remove(os.path.expanduser(path))
+
+# launch iTunes so that it creates new library files
+os.system("/Applications/iTunes.app/Contents/MacOS/iTunes &")
+os.system("sleep 3 && killall iTunes 2>/dev/null")
+
+exit(0)
+
 # generate an M3U playlist with all of the files
 m3u = make_m3u(os.path.expanduser("~/Desktop/music"))
-print(m3u)
+
+if os.path.isfile("/tmp/music.m3u"):
+	os.remove("/tmp/music.m3u")
+
+with open("/tmp/music.m3u", "w") as file:
+	file.write(m3u)
+
+
